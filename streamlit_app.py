51228a137ffb26bb196dd348c3e8f7bf7f3d6555
vasiliyaromenka <vasili.yaromenka@gmail.com>
2023-06-17 22:53:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+   
+streamlit.header ('Breakfast Favorites')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text ( '🥑🍞 Avocado Toasts')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
